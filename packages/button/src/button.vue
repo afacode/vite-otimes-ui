6d0d2861cb73dd3a41e0d679b1efa322d17f7416
afacode <afacode@outlook.com>
2021-07-15 22:19:03 +0800

    feat: Button done

--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -1,33 +1,39 @@
 <template>
   <button :class="[
     'el-button',
-    type ? 'el-button--' + type : '',
+    `el-button--${type}`,
     buttonSize ? 'el-button--' + buttonSize : '',
     {
       'is-disabled': buttonDisabled,
       'is-loading': loading,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
       }
-    ]"
-    :disabled="buttonDisabled || loading"
-    @click="handleClick"
-  >
+    ]" :disabled="buttonDisabled || loading" :autofocus="autofocus" :type="nativeType" @click="handleClick">
     <i v-if="loading" class="el-icon-loading"></i>
-    <span v-if="$slots.default"><slot></slot></span>
+    <i v-if="icon" :class="icon"></i>
+    <span v-if="$slots.default">
+      <slot></slot>
+    </span>
   </button>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from "vue"
+import { defineComponent, computed } from 'vue'
 import type { PropType } from 'vue'
 
-type ButtonType = PropType<'primary' | 'success' | 'warning' | 'danger' | 'info' | 'text' | 'default'>
+type ButtonType = PropType<
+  'primary' | 'success' | 'warning' | 'danger' | 'info' | 'text' | 'default'
+>
+type ButtonNativeType = PropType<'button' | 'submit' | 'reset'>
 
 export default defineComponent({
   name: 'OButton',
   props: {
     type: {
       type: String as ButtonType,
-      default: 'default',
+      default: 'default'
     },
     buttonSize: {
       type: String
@@ -36,12 +42,36 @@ export default defineComponent({
       type: Boolean,
       default: false
     },
+    plain: {
+      type: Boolean,
+      default: false
+    },
+    round: {
+      type: Boolean,
+      default: false
+    },
+    circle: {
+      type: Boolean,
+      default: false
+    },
+    autofocus: Boolean,
+    icon: {
+      type: String,
+      default: ''
+    },
     disabled: {
       type: Boolean,
       default: false
     },
+    nativeType: {
+      type: String as ButtonNativeType,
+      default: 'button',
+      validator: (val: string) => {
+        return ['button', 'submit', 'reset'].includes(val)
+      }
+    }
   },
-  emit: ['click'],
+  emits: ['click'],
   setup(props, ctx) {
     const buttonDisabled = computed(() => {
       return props.disabled
