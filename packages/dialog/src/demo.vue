54b57b7db6c1613af016508ad74a21615dfdbe0e
afacode <afacode@outlook.com>
2021-07-20 09:40:03 +0800

    feat: input component

---
+++ packages/dialog/src/demo.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <o-dialog title="title"></o-dialog>
+  <o-dialog>
+    <template v-slot:title>
+      <h2>template title</h2>
+    </template>
+  </o-dialog>
+</div>
+</template>
+
+<script lang="ts" setup>
+  import ODialog from './dialog.vue'
+</script>
+
--- packages/input/src/input.vue
+++ packages/input/src/input.vue
@@ -1,16 +1,59 @@
 <template>
   <div>
-    <slot></slot>
+    <input
+      class
+      :class="{ 'is-disabled': disabled }"
+      :placeholder="placeholder"
+      :type="type"
+      :name="name"
+      :value="value"
+      @input="handleInput"
+    />
+    <span>
+      <i class></i>
+    </span>
   </div>
 </template>
 
 <script lang='ts'>
-import { defineComponent } from 'vue'
+interface BooleanFalseType {
+  type: Boolean,
+  default?: false
+}
+import { defineComponent, PropType } from 'vue'
+import { ElementNode } from '@vue/compiler-dom';
 export default defineComponent({
   name: 'OInput',
-  props: { },
-  setup(props) {
+  props: {
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    name: {
+      type: String,
+      default: ''
+    },
+    value: null,
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    clearable: {
+      type: Boolean,
+      default: false
+    },
+    password: Object as PropType<BooleanFalseTyp>
+  },
+  emits: ['input'],
+  setup(props, ctx) {
     // init here
+    const handleInput = (e: any) => {
+      ctx.emit('input', e.target.value)
+    }
   },
 })
 </script>
